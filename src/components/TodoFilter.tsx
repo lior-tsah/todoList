92c0eff0cac7d620b983d91e4ed84eb32e89f995
Lior Tsah <liortsah@gmail.com>
2024-10-27 20:50:26 +0200

    fix naming

--- src/components/TodoFilter.tsx
+++ src/components/TodoFilter.tsx
@@ -2,15 +2,12 @@ import { useEffect, useState } from "react";
 import { FormControl, InputLabel, MenuItem, Select } from "@mui/material";
 import { Task } from "../models/Models";
 
-interface VisibilityFilterProps {
+interface Props {
   allTasks: Task[];
   setFilteredData: (val: Task[]) => void;
 }
 
-const TodoFilter: React.FC<VisibilityFilterProps> = ({
-  allTasks,
-  setFilteredData,
-}: VisibilityFilterProps) => {
+const TodoFilter: React.FC<Props> = ({ allTasks, setFilteredData }) => {
   const [inputValue, setInputValue] = useState("All");
 
   const onClickFilter = (input: string) => {
@@ -32,30 +29,28 @@ const TodoFilter: React.FC<VisibilityFilterProps> = ({
   useEffect(() => {
     onClickFilter(inputValue);
   }, [allTasks]);
-  
+
   return (
-   
-        <FormControl sx={{ m: 1, minWidth: 150 }}>
-          <InputLabel
-            // sx={{ top: -4 }}
-            id="demo-simple-select-label"
-          >
-            Filter by
-          </InputLabel>
-          <Select
-            labelId="demo-simple-select-label"
-            label="Filter by"
-            value={inputValue}
-            defaultValue="All"
-            onChange={(e) => onClickFilter(e.target.value)}
-            sx={{ height: 40, width: 150 }}
-          >
-            <MenuItem value={"All"}>All</MenuItem>
-            <MenuItem value={"Completed"}>Completed</MenuItem>
-            <MenuItem value={"Incompleted"}>Incompleted</MenuItem>
-          </Select>
-        </FormControl>
-     
+    <FormControl sx={{ m: 1, minWidth: 150 }}>
+      <InputLabel
+        // sx={{ top: -4 }}
+        id="demo-simple-select-label"
+      >
+        Filter by
+      </InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        label="Filter by"
+        value={inputValue}
+        defaultValue="All"
+        onChange={(e) => onClickFilter(e.target.value)}
+        sx={{ height: 40, width: 150 }}
+      >
+        <MenuItem value={"All"}>All</MenuItem>
+        <MenuItem value={"Completed"}>Completed</MenuItem>
+        <MenuItem value={"Incompleted"}>Incompleted</MenuItem>
+      </Select>
+    </FormControl>
   );
 };
 export default TodoFilter;
